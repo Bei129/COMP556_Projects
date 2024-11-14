d3b016a9f607d13484dda24d07233da92fc45661
parishermit <993566742@qq.com>
2024-11-13 19:29:02 -0600

    update DV

--- project3/RoutingProtocolImpl.cc
+++ project3/RoutingProtocolImpl.cc
@@ -34,6 +34,11 @@ void RoutingProtocolImpl::init(unsigned short num_ports, unsigned short router_i
 
     sys->set_alarm(this, 0, (void *)ALARM_PING);
     sys->set_alarm(this, CHECK_DURATION, (void *)ALARM_CHECK);
+
+    // DV
+    // 目前写的硬编码3和4，后面应该要改
+    sys->set_alarm(this, 0, (void *)3);
+    sys->set_alarm(this, 1000, (void *)4);
 }
 
 void *RoutingProtocolImpl::create_packet(unsigned short type, unsigned short size)
@@ -74,9 +79,9 @@ void RoutingProtocolImpl::handle_ping(unsigned short port, void *packet)
     pong_header->dst = ping_header->src;
 
     sys->send(port, pong_packet, ping_header->size);
-
 }
 
+// 在链路状态改变时要触发DV更新,还没写这一块
 void RoutingProtocolImpl::handle_pong(unsigned short port, void *packet)
 {
     struct packet *header = (struct packet *)packet;
@@ -99,6 +104,7 @@ void RoutingProtocolImpl::handle_pong(unsigned short port, void *packet)
     }
 }
 
+// 在链路断开时还要更新DV，还没写
 void RoutingProtocolImpl::check_neighbor_status()
 {
     unsigned int current_time = sys->time();
@@ -121,8 +127,7 @@ void RoutingProtocolImpl::check_neighbor_status()
 
 void RoutingProtocolImpl::handle_alarm(void *data)
 {
-
-    intptr_t alarm_type = (intptr_t)data;
+    long alarm_type = (long)data;
 
     if (alarm_type == ALARM_PING)
     { // PING alarm
@@ -136,6 +141,143 @@ void RoutingProtocolImpl::handle_alarm(void *data)
     { // CHECK alarm
         check_neighbor_status();
         sys->set_alarm(this, 1000, (void *)2);
+    }
+    // 目前写的硬编码3和4
+    else if (alarm_type == 3)
+    {
+        send_dv_update(false);
+        sys->set_alarm(this, 30000, (void *)3); // 30秒后再次更新
+    }
+    else if (alarm_type == 4)
+    {
+        check_routes_timeout();
+        sys->set_alarm(this, 1000, (void *)4); // 1秒后再次检查
+    }
+}
+
+void RoutingProtocolImpl::send_dv_update(bool triggered)
+{
+    // DV包：包头 + 每个路由条目(destination + cost)
+    unsigned short num_routes = routing_table.size();
+    unsigned short packet_size = sizeof(struct packet) +
+                                 num_routes * (sizeof(unsigned short) * 2);
+
+    for (unsigned short port = 0; port < num_ports; port++)
+    {
+        if (!ports[port].isAlive)
+            continue;
+
+        char *dv_packet = new char[packet_size];
+        struct packet *pkt = (struct packet *)dv_packet;
+        pkt->type = DV;
+        pkt->src = router_id;
+        pkt->dst = 0;
+        pkt->size = packet_size;
+
+        unsigned short *payload = (unsigned short *)(dv_packet + sizeof(struct packet));
+        int entry_index = 0;
+
+        for (auto const &route : routing_table)
+        {
+            if (!route.second.valid)
+                continue;
+
+            // 如果下一跳是该端口的邻居，成本无限大
+            unsigned int cost = route.second.cost;
+            if (route.second.port == port)
+            {
+                cost = INFINITY_COST;
+            }
+
+            payload[entry_index++] = htons(route.first); // destination
+            payload[entry_index++] = htons(cost);        // cost
+        }
+
+        sys->send(port, dv_packet, packet_size);
+    }
+}
+
+void RoutingProtocolImpl::update_route(unsigned short dest, unsigned short next_hop,
+                                       unsigned short port, unsigned int cost)
+{
+    auto &route = routing_table[dest];
+    route.destination = dest;
+    route.next_hop = next_hop;
+    route.port = port;
+    route.cost = cost;
+    route.last_update = sys->time();
+    route.valid = true;
+}
+
+void RoutingProtocolImpl::handle_dv_packet(unsigned short port, void *packet)
+{
+    struct packet *pkt = (struct packet *)packet;
+    unsigned short src_id = pkt->src;
+    unsigned short *payload = (unsigned short *)((char *)packet + sizeof(struct packet));
+    int num_entries = (pkt->size - sizeof(struct packet)) / (sizeof(unsigned short) * 2);
+
+    bool route_changed = false;
+
+    // 处理每个路由条目
+    for (int i = 0; i < num_entries; i++)
+    {
+        unsigned short dest = ntohs(payload[i * 2]);
+        unsigned int cost = ntohs(payload[i * 2 + 1]);
+
+        if (cost == INFINITY_COST)
+            continue;
+
+        // 总成本
+        unsigned int total_cost = cost + ports[port].cost;
+
+        auto it = routing_table.find(dest);
+        if (it == routing_table.end() || !it->second.valid)
+        {
+            if (total_cost < INFINITY_COST)
+            {
+                update_route(dest, src_id, port, total_cost);
+                route_changed = true;
+            }
+        }
+        else if (it->second.next_hop == src_id)
+        {
+            if (total_cost != it->second.cost)
+            {
+                update_route(dest, src_id, port, total_cost);
+                route_changed = true;
+            }
+        }
+        else if (total_cost < it->second.cost)
+        {
+            update_route(dest, src_id, port, total_cost);
+            route_changed = true;
+        }
+    }
+
+    if (route_changed)
+    {
+        send_dv_update(true);
+    }
+}
+
+void RoutingProtocolImpl::check_routes_timeout()
+{
+    bool route_changed = false;
+    unsigned int current_time = sys->time();
+
+    for (auto &route : routing_table)
+    {
+        if (route.second.valid &&
+            current_time - route.second.last_update > 45000)
+        { // 45秒超时
+            route.second.valid = false;
+            route_changed = true;
+        }
+    }
+
+    if (route_changed)
+    {
+        send_dv_update(true);
     }
 }
 
@@ -155,7 +297,7 @@ void RoutingProtocolImpl::recv(unsigned short port, void *packet, unsigned short
     }
 
     struct packet *header = (struct packet *)packet;
-    // 后面的 DV 接着这里写
+
     switch (header->type)
     {
     case PING:
@@ -163,6 +305,9 @@ void RoutingProtocolImpl::recv(unsigned short port, void *packet, unsigned short
         break;
     case PONG:
         handle_pong(port, packet);
+        break;
+    case DV:
+        handle_dv_packet(port, packet);
         break;
     default:
         printf("Unknown packet type received: %d\n", header->type);
--- project3/RoutingProtocolImpl.h
+++ project3/RoutingProtocolImpl.h
@@ -5,56 +5,76 @@
 #include "global.h"
 #include "RoutingProtocol.h"
 #include <vector>
+#include <map>
 
-struct packet {
-    unsigned char type;       
-    unsigned char reserved;   
-    unsigned short size;       
-    unsigned short src;       
-    unsigned short dst;        
+struct packet
+{
+    unsigned char type;
+    unsigned char reserved;
+    unsigned short size;
+    unsigned short src;
+    unsigned short dst;
 };
 
-struct PortStatus {
-    bool isAlive;             
-    unsigned short neighborId; 
-    unsigned int lastPongTime; 
-    unsigned short cost;       
+struct PortStatus
+{
+    bool isAlive;
+    unsigned short neighborId;
+    unsigned int lastPongTime;
+    unsigned short cost;
+};
+struct RouteEntry
+{
+    unsigned short destination;
+    unsigned short next_hop;
+    unsigned short port;
+    unsigned int cost;
+    unsigned int last_update;
+    bool valid;
 };
 
-class RoutingProtocolImpl : public RoutingProtocol {
-    public:
-        RoutingProtocolImpl(Node *n);
-        ~RoutingProtocolImpl();
+class RoutingProtocolImpl : public RoutingProtocol
+{
+public:
+    RoutingProtocolImpl(Node *n);
+    ~RoutingProtocolImpl();
 
-        void init(unsigned short num_ports, unsigned short router_id, 
-                 eProtocolType protocol_type);  
-        void handle_alarm(void *data);
-        void recv(unsigned short port, void *packet, unsigned short size);
+    void init(unsigned short num_ports, unsigned short router_id,
+              eProtocolType protocol_type);
+    void handle_alarm(void *data);
+    void recv(unsigned short port, void *packet, unsigned short size);
 
-    private:
-        Node *sys;                  
-        unsigned short router_id;   
-        unsigned short num_ports;   
-        std::vector<PortStatus> ports;  
+private:
+    Node *sys;
+    unsigned short router_id;
+    unsigned short num_ports;
+    std::vector<PortStatus> ports;
 
-        // 包大小相关常量 这里是12
-        static const unsigned short PING_PONG_PACK_SIZE = sizeof(struct packet) + sizeof(unsigned int);
-        
-        // 时间相关常量
-        static const unsigned int PONG_TIMEOUT = 15000;    // 15s neighbor timeout
-        static const unsigned int PING_DURATION = 10000;   // 10s ping interval  
-        static const unsigned int CHECK_DURATION = 1000;   // 1s check interval
+    // 包大小相关常量 这里是12
+    static const unsigned short PING_PONG_PACK_SIZE = sizeof(struct packet) + sizeof(unsigned int);
 
-        // Alarm类型常量
-        static const int ALARM_PING = 1;
-        static const int ALARM_CHECK = 2;
+    // 时间相关常量
+    static const unsigned int PONG_TIMEOUT = 15000;  // 15s neighbor timeout
+    static const unsigned int PING_DURATION = 10000; // 10s ping interval
+    static const unsigned int CHECK_DURATION = 1000; // 1s check interval
 
-        void send_ping(unsigned short port);
-        void handle_ping(unsigned short port, void *packet);
-        void handle_pong(unsigned short port, void *packet);
-        void check_neighbor_status();
-        void print_port_status();
-        void* create_packet(unsigned short type, unsigned short size);
+    // Alarm类型常量
+    static const int ALARM_PING = 1;
+    static const int ALARM_CHECK = 2;
+
+    void send_ping(unsigned short port);
+    void handle_ping(unsigned short port, void *packet);
+    void handle_pong(unsigned short port, void *packet);
+    void check_neighbor_status();
+    void print_port_status();
+    void *create_packet(unsigned short type, unsigned short size);
+
+    std::map<unsigned short, RouteEntry> routing_table;
+    void send_dv_update(bool triggered = false);
+    void handle_dv_packet(unsigned short port, void *packet);
+    void check_routes_timeout();
+    void update_route(unsigned short dest, unsigned short next_hop,
+                      unsigned short port, unsigned int cost);
 };
 
 #endif
